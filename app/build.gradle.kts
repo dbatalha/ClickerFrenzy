f5225357f8db64f4ce5df172d69a1986a7627603
dbatalha <daniel.p.batalha@sapo.pt>
2023-11-06 14:59:53 +0000

    Add notification every 1000 clicks.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("org.projectlombok:lombok:1.18.30")
     implementation("com.android.volley:volley:1.2.1")
+    implementation("androidx.work:work-runtime:2.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/templateapp/MainActivity.java
+++ app/src/main/java/com/example/templateapp/MainActivity.java
@@ -1,39 +1,41 @@
 package com.example.templateapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.NotificationCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TEST_CHANNEL = "test-channel";
+    private static final int THRESHOLD = 1000;
     private long timeEpoch = 0;
-
     private static final String ERROR_API = "error calling quotes api";
-
     private String url = "https://type.fit/api/quotes";
-
+    private int notificationStatus = 0;
     List<Quote> quotes;
 
     public MainActivity() {
@@ -58,16 +60,16 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.start();
     }
 
-    private void vibrate(int time){
+    private void vibrate(int time) {
         Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
-        if (vibrator.hasVibrator()){
+        if (vibrator.hasVibrator()) {
             vibrator.vibrate(time);
         }
 
     }
 
-    private void setRandomQuote(){
+    private void setRandomQuote() {
         int randomQuote = Helpers.getRandomNumber(0, quotes.size());
 
         Quote selectedQuote = quotes.get(randomQuote);
@@ -79,17 +81,18 @@ public class MainActivity extends AppCompatActivity {
         quoteAuthor.setText(selectedQuote.author);
     }
 
-    private void quotesRequest(){
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+    private void quotesRequest() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
             @Override
-                public void onResponse(JSONArray response) {
+            public void onResponse(JSONArray response) {
 
                 String text;
                 String author;
 
                 quotes = new ArrayList<>();
 
-                for (int i=0 ; i < response.length() ; i++){
+                for (int i = 0; i < response.length(); i++) {
                     try {
                         JSONObject obj = response.getJSONObject(i);
                         text = obj.getString("text");
@@ -108,11 +111,11 @@ public class MainActivity extends AppCompatActivity {
                 setRandomQuote();
 
             }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("REST", ERROR_API);
-                }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("REST", ERROR_API);
+            }
         });
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -121,23 +124,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void timeOperation() throws InterruptedException {
-        while (true){
+        while (true) {
 
-            if (this.timeEpoch == 0){
+            if (this.timeEpoch == 0) {
                 TimeUnit.SECONDS.sleep(1);
                 continue;
             }
 
             long currentTime = System.currentTimeMillis();
             long total = currentTime - timeEpoch;
-            if (total >= 30000){
+            if (total >= 30000) {
                 playWarningEffect();
                 TextView editText = (TextView) findViewById(R.id.counterTextLabel);
                 vibrate(50);
                 int currentClicks = ClickActions.decreaseClicks(editText, 1);
 
                 // When the clicks is 0 stop click and vibration.
-                if (currentClicks == 0){
+                if (currentClicks == 0) {
                     this.timeEpoch = 0;
                 }
             }
@@ -146,6 +149,55 @@ public class MainActivity extends AppCompatActivity {
             Log.d("THREAD", "Time calculation thread %s".format(Long.toString(total)));
         }
 
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = TEST_CHANNEL;
+            String description = "This is a test channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(TEST_CHANNEL, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void addNotification(String title, String value) {
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, TEST_CHANNEL)
+                        .setSmallIcon(R.drawable.cookie)
+                        .setContentTitle(title)
+                        .setContentText(value)
+                        .setAutoCancel(true)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Intent notificationIntent = new Intent(this, NotificationView.class);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        notificationIntent.putExtra(title, value);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1,
+                notificationIntent,
+                PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+
+    private void rewards(TextView textView){
+        int value = Integer.parseInt(textView.getText().toString());
+
+        if (value >= ( notificationStatus + THRESHOLD )){
+            addNotification("Clicker Frenzy",
+                    String.format("Congratulations you're reach %s", String.valueOf(value)));
+            notificationStatus = value;
+        }
     }
 
     @Override
@@ -157,7 +209,6 @@ public class MainActivity extends AppCompatActivity {
         editText.setText("0");
 
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
         this.quotesRequest();
 
         Thread timeThread = new Thread(new Runnable() {
@@ -182,6 +233,7 @@ public class MainActivity extends AppCompatActivity {
                 vibrate(100);
                 playSoundClick();
                 setRandomQuote();
+                rewards(editText);
             }
         });
         button.setOnLongClickListener(new View.OnLongClickListener() {
@@ -193,6 +245,7 @@ public class MainActivity extends AppCompatActivity {
                 vibrate(500);
                 ClickActions.increaseClicks(editText, 15);
                 setRandomQuote();
+                rewards(editText);
                 return true;
             }
         });
